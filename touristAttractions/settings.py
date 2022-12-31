6d53042a11e60c2a25d402f0dd4d338c24aabed8
aldairdiaz <irving.s21diaz@gmail.com>
2022-12-30 19:30:36 -0600

    Initial Commit

--- touristAttractions/settings.py
+++ touristAttractions/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,11 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-6@_ssp0=-**a0g7ijgz$q0sjp5-n(4#u69=nl*cura5oxh968d'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+# change to env:
 ALLOWED_HOSTS = [
     "192.168.1.194",
     "127.0.0.1"
@@ -58,8 +60,7 @@ ROOT_URLCONF = 'touristAttractions.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR / 'templates']
-        ,
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
