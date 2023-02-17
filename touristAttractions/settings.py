bdb1000771fae22357dd8ae3e514c2806519099b
aldairdiaz <irving.s21diaz@gmail.com>
2023-02-17 14:22:11 -0600

    "Implement Forms"

--- touristAttractions/settings.py
+++ touristAttractions/settings.py
@@ -21,16 +21,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get("SECRET_KEY")
+SECRET_KEY = 'os.environ.get("SECRET_KEY")'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
 # change to env:
-ALLOWED_HOSTS = [
-    "192.168.1.194",
-    "127.0.0.1"
-]
+ALLOWED_HOSTS = []
 
 
 # Application definition
--- tourist_attractions/views.py
+++ tourist_attractions/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
-
-# Create your views here.
-# This is the dictionary for all the attractions
-attractions = [
-    {'attraction_name': 'Niagara Falls', 'state': 'New York'},
-    {'attraction_name': 'Grand Canyon National Park', 'state': 'Arizona'},
-    {'attraction_name': 'Mall of America', 'state': 'Minnesota'},
-    {'attraction_name': 'Mount Rushmore', 'state': 'South Dakota'},
-    {'attraction_name': 'Times Square', 'state': 'New York'},
-    {'attraction_name': 'Walt Disney World', 'state': 'Florida'}
-]
+from .models import State, Attraction
 
 
 def home(request):
     # The context is all the variables we want passed into the template.
-    context = {"attractions": attractions}
+    all_attractions = Attraction.objects.all()
+    context = {"attractions": all_attractions}
     return render(request, 'touristAttractions/home.html', context)
 
 
-def details(request, statename):
+def details(request, state_name):
     # We replace the dash with a space for later ease of use. The dash is there because of the slugify filter.
-    context = {"attractions": attractions, "statename": statename.replace("-", " ")}
+    attractions = Attraction.objects.all()
+    context = {"attractions": attractions, "state_name": state_name.replace("-", " ")}
     return render(request, 'touristAttractions/details.html', context)
